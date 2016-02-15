6a053a1bdb92079a4bfee15c538ad6e02294886f
Miguel Amigot <miguel.amigot@gmail.com>
2016-02-14 22:24:45 -0500

    More populated route logic

--- app.py
+++ app.py
@@ -1,46 +1,83 @@
 from functools import wraps
-from flask import Flask, session, render_template, request, redirect, url_for
-from storage import access
+from flask import Flask, g, session, render_template, request, redirect, url_for
+import storage
 
 app = Flask(__name__)
 
 def login_required(f):
-    """Decorator that checks the validity of a session (and that it's there)"""
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if g.user is None:
+        if session.get('username') is None:
             return redirect(url_for('login', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    pass
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('register.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    if request.method == 'POST':
+        user = storage.User(request.form['username'], request.form['password'])
+
+        if user.verify_credentials():
+            session['username'] = user.username
+            return redirect(url_for('timeline'))
+
+    return render_template('login.html')
+
+@app.route('/signout', methods=['POST'])
+def signout():
+    session.pop('username')
+    return redirect(url_for('login'))
 
 @app.route('/')
-@login_required
-def index():
-    pass
-
 @app.route('/timeline')
 @login_required
 def timeline():
-    pass
+    print(session.get('username'))
+    username = session.get('username')
 
-@app.route('/profile/<user_id>')
+    posts = []
+    for i in range(10):
+        posts.append(storage.Post(text='jajajajaja', username='joe', timestamp='123456790'))
+
+    return render_template('timeline.html', username=username, posts=posts)
+
+@app.route('/profile')
+@app.route('/profile/<secondary_username>', methods=['GET', 'POST'])
 @login_required
-def profile(user_id=None):
-    pass
+def profile(secondary_username=None):
+    if request.method == 'GET':
+        # Get the user's profile
+        username = session.get('username')
+        return render_template('profile.html', username=username, secondary_username=secondary_username)
+    else:
+        # Modify the user's relationship with somebody else (follow, unfollow, etc.)
+        pass
 
-@app.route('/status/', methods=['POST', 'DELETE'])
+@app.route('/post', methods=['GET', 'POST', 'PUT', 'DELETE'])
 @login_required
-def status():
-    pass
-
+def post():
+    if request.method == 'GET':
+        # Get
+        return render_template('post.html')
+    elif request.method == 'POST':
+        # Save
+        pass
+    elif request.method == 'PUT':
+        # Update
+        pass
+    else:
+        # Delete
+        pass
 
 if __name__ == '__main__':
+    app.secret_key = 'badsecretkey'
+    app.debug = True
+
     app.run()
