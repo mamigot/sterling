8f0a9ecc18174faed5b2a0d6184ce6161131261f
Miguel Amigot <miguel.amigot@gmail.com>
2016-02-18 23:36:06 -0500

    Unnecessary functions; file.seek() calls from the end are more efficient than this home-cooked solution

--- storage/utils/file_handler.py
+++ storage/utils/file_handler.py
@@ -13,35 +13,3 @@ def get_path(username, stored_type):
 
     filename = '%s_%d.txt' % (filetype, filenumber)
     return os.path.join(STORAGE_ROOT_PATH, filename)
-
-def read_chunks_backwards(filepath, chunk_size):
-    """Read one chunk at a time, backwards"""
-    bytes_left = os.path.getsize(filepath)
-
-    with open(filepath, 'r') as f:
-        while True:
-            if bytes_left > 0:
-                f.seek(-1 * bytes_left, os.SEEK_END)
-                yield f.read(chunk_size)
-
-                bytes_left -= min(bytes_left, chunk_size)
-            else:
-                break
-
-def read_entries_backwards(filepath, entry_size):
-    desired_chunk = 4096
-    chunk_size = entry_size
-
-    while (chunk_size + entry_size) <= desired_chunk:
-        chunk_size += entry_size
-
-    entries_for_chunk = []
-
-    for chunk in read_chunks_backwards(filepath, chunk_size):
-        for i in range(0, chunk_size, entry_size):
-            entry = chunk[i:(i + entry_size)]
-
-            if entry.strip():
-                entries_for_chunk.append(entry)
-
-    return reversed(entries_for_chunk)
