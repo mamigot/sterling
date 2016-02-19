6a40f318e77f13cab31c05e00cf802b86035ef03
Miguel Amigot <miguel.amigot@gmail.com>
2016-02-19 18:20:44 -0500

    Added func to modify the 'active' bytes in a file (more efficient than having to do so in two steps)

--- storage/utils/file_handler.py
+++ storage/utils/file_handler.py
@@ -29,6 +29,8 @@ def item_match(file_path, item_size, compare_func, compare_kwargs={}):
 
             read_ptr += item_size
 
+    return None
+
 def item_match_sweep(file_path, item_size, compare_func, compare_kwargs={}, limit=None):
     file_size = os.path.getsize(file_path)
     items = []
@@ -49,3 +51,26 @@ def item_match_sweep(file_path, item_size, compare_func, compare_kwargs={}, limi
             read_ptr += item_size
 
     return items
+
+def set_active_flag(active_flag, file_path, item_size, compare_func, compare_kwargs):
+    file_size = os.path.getsize(file_path)
+    active_byte = ('1' if active_flag == True else '0').encode('utf-8')
+
+    num_modified = 0
+
+    with open(file_path, 'rb+') as f:
+        read_ptr = item_size
+
+        while abs(read_ptr) <= abs(file_size):
+            f.seek(-read_ptr, os.SEEK_END)
+            item = f.read(item_size).decode('utf-8')
+
+            if compare_func(item, **compare_kwargs):
+                f.seek(-read_ptr, os.SEEK_END)
+                f.write(active_byte)
+                f.seek(-read_ptr, os.SEEK_END)
+                num_modified += 1
+
+            read_ptr += item_size
+
+    return num_modified
