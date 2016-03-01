57c7d6299869ef93710844172855177fbc71d058
Miguel Amigot <miguel.amigot@gmail.com>
2016-03-01 17:29:30 -0500

    Removed unused URL param on login decorator

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get('username') is None:
-            return redirect(url_for('login', next=request.url))
+            return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
 
