eb267d43c055a1b07e5fb5f59c00ac087d4f5296
Miguel Amigot <miguel.amigot@gmail.com>
2016-03-16 01:06:37 -0400

    Only function that's relevant to the outside is configServer() --takes care of all

--- storage/include/config.h
+++ storage/include/config.h
@@ -9,5 +9,6 @@ extern map<string, int> configParams;
 // Absolute path to the user data files of the application
 extern string STORAGE_FILES_PATH;
 
-// Create the user data files of the application
-void initiateStorage(void);
+// Before each session: creates the user data files of the application if they
+// don't exist and makes other relevant params available
+void configServer(void);
