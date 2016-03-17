461255460b206e4b6f5bc317f0969231117310df
Miguel Amigot <miguel.amigot@gmail.com>
2016-03-17 18:09:57 -0400

    Another helper func

--- storage/include/utils.h
+++ storage/include/utils.h
@@ -8,4 +8,6 @@ using namespace std;
 // True if str starts with match
 bool startswith(const string& str, const string& match);
 
+string getTimeNow(void);
+
 #endif
--- storage/src/utils.cpp
+++ storage/src/utils.cpp
@@ -1,7 +1,18 @@
+#include <sstream>
+#include <ctime>
 #include "utils.h"
 using namespace std;
 
 
 bool startswith(const string& str, const string& match){
   return str.substr(0, match.length()).compare(match) == 0;
+}
+
+string getTimeNow(void){
+  time_t now = time(NULL);
+
+  stringstream strm;
+  strm << now;
+
+  return strm.str();
 }
