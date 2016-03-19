f5961a682f71aaa61b447c03e8dc812d8c129bff
Miguel Amigot <miguel.amigot@gmail.com>
2016-03-19 11:51:48 -0400

    \w regex not supported... use [a-z], etc.

--- storage/src/protocol.cpp
+++ storage/src/protocol.cpp
@@ -16,51 +16,51 @@ using namespace std;
 regex reGet("GET/.*");
 
 // GET/credential/username\0
-regex reExists("GET/credential/([\\w]+)\0");
+regex reExists("GET/credential/([a-z]+)\0");
 
 // GET/credential/username:password\0
-regex reVerifyCredential("GET/credential/([\\w]+):([\\w]+)\0");
+regex reVerifyCredential("GET/credential/([a-z]+):([a-z]+)\0");
 
 // GET/posts/profile/username:limit\0
-regex reGetProfilePosts("GET/posts/profile/([\\w]+):(-?[\\d]+)\0");
+regex reGetProfilePosts("GET/posts/profile/([a-z]+):(-?[0-9]+)\0");
 
 // GET/posts/timeline/username:limit\0
-regex reGetTimelinePosts("GET/posts/timeline/([\\w]+):(-?[\\d]+)\0");
+regex reGetTimelinePosts("GET/posts/timeline/([a-z]+):(-?[0-9]+)\0");
 
 // GET/relations/username:friendUsername\0
-regex reIsFollowing("GET/relations/([\\w]+):([\\w]+)\0");
+regex reIsFollowing("GET/relations/([a-z]+):([a-z]+)\0");
 
 // GET/relations/followers/username:limit\0
-regex reGetFollowers("GET/relations/followers/([\\w]+):(-?[\\d]+)\0");
+regex reGetFollowers("GET/relations/followers/([a-z]+):(-?[0-9]+)\0");
 
 // GET/relations/friends/username:limit\0
-regex reGetFriends("GET/relations/friends/([\\w]+):(-?[\\d]+)\0");
+regex reGetFriends("GET/relations/friends/([a-z]+):(-?[0-9]+)\0");
 
 
 /********** SAVE/... **********/
 regex reSave("SAVE/.*");
 
 // SAVE/credential/username:password\0
-regex reSaveCredential("SAVE/credential/([\\w]+):([\\w]+)\0");
+regex reSaveCredential("SAVE/credential/([a-z]+):([a-z]+)\0");
 
 // SAVE/posts/username:text\0
-regex reSavePost("SAVE/posts/([\\w]+):(.*)\0");
+regex reSavePost("SAVE/posts/([a-z]+):(.*)\0");
 
 // SAVE/relations/username:friendUsername\0
-regex reFollow("SAVE/relations/([\\w]+):([\\w]+)\0");
+regex reFollow("SAVE/relations/([a-z]+):([a-z]+)\0");
 
 
 /********** DELETE/... **********/
 regex reDelete("DELETE/.*");
 
 // DELETE/credential/username:password\0
-regex reDeleteCredential("DELETE/credential/([\\w]+):([\\w]+)\0");
+regex reDeleteCredential("DELETE/credential/([a-z]+):([a-z]+)\0");
 
 // DELETE/posts/username:timestamp\0
-regex reDeletePost("DELETE/posts/([\\w]+):([\\d]{10})\0");
+regex reDeletePost("DELETE/posts/([a-z]+):([0-9]{10})\0");
 
 // DELETE/relations/username:friendUsername\0
-regex reUnfollow("DELETE/relations/([\\w]+):([\\w]+)\0");
+regex reUnfollow("DELETE/relations/([a-z]+):([a-z]+)\0");
 
 
 void initiateProtocol(void){
