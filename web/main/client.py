0a6faaa1fb483764781ec91964f21d9f29a4f855
Miguel Amigot <miguel.amigot@gmail.com>
2016-04-02 23:26:57 -0400

    Avoid encoding issues while communicating from the server to the client by cutting the message short at the first UTF-8 control character

--- web/main/client.py
+++ web/main/client.py
@@ -30,7 +30,7 @@ def request(command, host=DATASERVER_HOST, port=DATASERVER_PORT):
     s.send(command.encode('utf-8'))
 
     # Learn how many packets are needed
-    header = s.recv(DATASERVER_BUFFSIZE).decode('utf-8')
+    header = parse_as_utf8(s.recv(DATASERVER_BUFFSIZE))
     match = re_successful_header.match(header)
 
     if match:
@@ -42,7 +42,7 @@ def request(command, host=DATASERVER_HOST, port=DATASERVER_PORT):
         # Receive as many packets as indicated
         output = ""
         for i in range(num_expected + 1):
-            output += s.recv(DATASERVER_BUFFSIZE).decode('utf-8')
+            output += parse_as_utf8(s.recv(DATASERVER_BUFFSIZE))
             # Signal that we have received the packet
             s.send(ClientResponseSignal.Ack)
 
@@ -50,3 +50,10 @@ def request(command, host=DATASERVER_HOST, port=DATASERVER_PORT):
 
     elif header.startswith('500'):
         raise ErrorRetrievingFromServer()
+
+def parse_as_utf8(buffer_bytes):
+    """Read until we encounter the '\x00' (control character) in UTF-8. Assumes
+    that read bytes will be terminated with this. Then, decode accordingly."""
+
+    end_idx = buffer_bytes.find(b'\x00')
+    return (buffer_bytes[:end_idx]).decode('utf-8')
