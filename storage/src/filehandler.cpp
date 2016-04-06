5f404b405dce86e830ef015f6efd0712f1ff31d2
Miguel Amigot <miguel.amigot@gmail.com>
2016-04-06 11:51:15 -0400

    Defined an iterator class for the data files (refactoring)

--- storage/src/filehandler.cpp
+++ storage/src/filehandler.cpp
@@ -1,4 +1,5 @@
 #include <sstream>
+#include <iostream> // used only for debugging
 #include <stdexcept>
 #include "serializers.h"
 #include "utils.h"
@@ -6,128 +7,137 @@
 using namespace std;
 
 
-int itemMatch(const string& filePath, string& dataType, map<string, string> matchArgs){
+LReader::LReader(const string& filePath, const string& dataType) {
   // Make sure that the type of stored data in the application is valid
   if(!configParams.count("FILE_COUNT_" + dataType)){
     throw std::runtime_error("Given dataType is unknown");
   }
 
-  // How many characters each item takes up (will read one chunk at a time)
-  int fileSize = getFileSize(filePath);
+  cout << filePath << endl;
+  cout << dataType << endl;
+
+  fileSize = getFileSize(filePath);
+
+  // Size of each item in the file (allows to analyze one-by-one)
+  itemSize = configParams["SERIAL_SIZE_" + dataType];
 
   // Start from the bottom of the file
-  int itemSize = configParams["SERIAL_SIZE_" + dataType];
-  int readPtr = itemSize;
+  offsetFromEnd = 0;
 
-  FILE* matchedFile;
   matchedFile = fopen(filePath.c_str(), "r+");
+}
 
-  char item[itemSize];
+LReader::~LReader() { fclose(matchedFile); }
 
-  while(readPtr <= fileSize){
-    fseek(matchedFile, -readPtr, SEEK_END);
-    fread(item, itemSize, 1, matchedFile);
-    item[itemSize] = '\0'; // Cap the garbage (without this, garbage is appended)
+bool LReader::hasNext() {
+  return offsetFromEnd < fileSize;
+}
 
-    // Compare each item to its corresponding serialized version using the
-    // criteria listed by matchArgs
-    if(matchesSerialized(string(item), dataType, matchArgs)){
-      // Return the first match that is found
-      fclose(matchedFile);
-      return readPtr;
+string LReader::next() {
+  if(hasNext()){
+    offsetFromEnd += itemSize;
+    return readItem(offsetFromEnd);
+
+  }else{
+    return string();
+  }
+}
+
+bool LReader::hasPrev() {
+  return offsetFromEnd >= itemSize;
+}
+
+string LReader::prev() {
+  if(hasPrev()){
+    offsetFromEnd -= itemSize;
+    return readItem(offsetFromEnd);
+
+  }else{
+    return string();
+  }
+}
+
+int LReader::getItemSize() { return itemSize; }
+
+int LReader::getReadPtr() { return offsetFromEnd; }
+
+FILE* LReader::getFilePtr() { return matchedFile; }
+
+string LReader::readItem(int offsetFromEnd) {
+  char buff[itemSize];
+
+  fseek(matchedFile, -offsetFromEnd, SEEK_END);
+  fread(buff, itemSize, 1, matchedFile);
+
+  buff[itemSize] = '\0'; // Cap the garbage (without this, it is appended)
+  return string(buff);
+}
+
+int itemMatch(const string& filePath, string& dataType, map<string, string> matchArgs){
+  LReader reader = LReader(filePath, dataType);
+
+  while(reader.hasNext()){
+    auto item = reader.next();
+
+    if(matchesSerialized(item, dataType, matchArgs)){
+      return reader.getReadPtr();
     }
-
-    readPtr += itemSize;
   }
 
-  fclose(matchedFile);
   return -1;
 }
 
 vector<string> itemMatchSweep(const string& filePath, string& dataType, map<string, string> matchArgs, int limit){
-  // Make sure that the type of stored data in the application is valid
-  if(!configParams.count("FILE_COUNT_" + dataType)){
-    throw std::runtime_error("Given dataType is unknown");
-  }
-
-  // How many characters each item takes up (will read one chunk at a time)
-  int fileSize = getFileSize(filePath);
-
-  // Start from the bottom of the file
-  int itemSize = configParams["SERIAL_SIZE_" + dataType];
-  int readPtr = itemSize;
-
-  FILE* matchedFile;
-  matchedFile = fopen(filePath.c_str(), "r+");
-
-  char item[itemSize];
   vector<string> allMatches;
   if(!limit) return allMatches;
 
-  while(readPtr <= fileSize){
-    fseek(matchedFile, -readPtr, SEEK_END);
-    fread(item, itemSize, 1, matchedFile);
-    item[itemSize] = '\0'; // Cap the garbage (without this, garbage is appended)
+  LReader reader = LReader(filePath, dataType);
+
+  while(reader.hasNext()){
+    auto item = reader.next();
 
     // Compare each item to its corresponding serialized version using the
     // criteria listed by matchArgs
-    if(matchesSerialized(string(item), dataType, matchArgs)){
+    if(matchesSerialized(item, dataType, matchArgs)){
       // Append to the vector of known matches. Return if we have enough.
       // Interpret limit == -1 to mean "take all".
       allMatches.push_back(item);
 
-      if(limit != -1 && (int) allMatches.size() >= limit){
+      if(limit != -1 && allMatches.size() >= limit){
         break;
       }
     }
-
-    readPtr += itemSize;
   }
 
-  fclose(matchedFile);
-  //return allMatches.str();
   return allMatches;
 }
 
 int setActiveFlag(bool active, const string& filePath, string& dataType, map<string, string> matchArgs){
-  // Make sure that the type of stored data in the application is valid
-  if(!configParams.count("FILE_COUNT_" + dataType)){
-    throw std::runtime_error("Given dataType is unknown");
-  }
-
-  int fileSize = getFileSize(filePath);
-
-  // How many characters each item takes up (will read one chunk at a time)
-  int itemSize = configParams["SERIAL_SIZE_" + dataType];
-  int readPtr = itemSize;
-
-  FILE* matchedFile;
-  matchedFile = fopen(filePath.c_str(), "r+");
-
   // There's supposed to be a '1' if the item is active and a '0' if it's not
-  char activeByte = active ? '1' : '0';
+  char activeFlag = active ? '1' : '0';
   unsigned int numModified = 0;
-  char item[itemSize];
 
-  while(readPtr <= fileSize){
-    fseek(matchedFile, -readPtr, SEEK_END);
-    fread(item, itemSize, 1, matchedFile);
-    item[itemSize] = '\0'; // Cap the garbage (without this, garbage is appended)
+  LReader reader = LReader(filePath, dataType);
+
+  while(reader.hasNext()){
+    auto item = reader.next();
 
     // Compare each item to its corresponding serialized version using the
     // criteria listed by matchArgs
-    if(matchesSerialized(string(item), dataType, matchArgs)){
+    if(matchesSerialized(item, dataType, matchArgs)){
       // For each successful match, step back, modify the active bit to be
       // what was specified, and keep going.
-      fseek(matchedFile, -readPtr, SEEK_END);
-      fwrite(&activeByte, 1, 1, matchedFile);
-      fseek(matchedFile, -readPtr, SEEK_END);
+
+      auto filePtr = reader.getFilePtr();
+      auto currReadPtr = reader.getReadPtr();
+
+      fseek(filePtr, -currReadPtr, SEEK_END);
+      fwrite(&activeFlag, 1, 1, filePtr);
+      fseek(filePtr, -currReadPtr, SEEK_END);
+
       numModified++;
     }
-
-    readPtr += itemSize;
   }
 
-  fclose(matchedFile);
   return numModified;
 }
