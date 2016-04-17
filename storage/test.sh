f5db9ebf7306bfa1a37bf1e27b9ccb4d7b165150
Miguel Amigot <miguel.amigot@gmail.com>
2016-04-17 12:36:43 -0400

    Run to launch tests (some of them)

---
+++ storage/test.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#############################
+# System Requirements (!!!) #
+#############################
+
+# Version 4.9 or greater of g++
+echo "CHECK THAT YOU ARE USING VERSION 4.9 OF g++ OR GREATER USING 'g++ -version'."
+echo "IF YOU ARE USING AN OLDER VERSION, THE APPLICATION WILL NOT COMPILE."
+
+########################################################################
+# Parameters to communicate with the client servers (change as needed) #
+########################################################################
+
+export DATASERVER_PORT=13002
+
+export DATASERVER_BUFFSIZE=8192
+
+########################################
+# Application configuration parameters #
+########################################
+
+# The root of the project is the root of this script
+# http://stackoverflow.com/a/246128/2708484
+PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Make all root-level modules of the app accessible
+export PATH=$PATH:$PROJECT_ROOT/main
+
+# Application configuration variables
+export CONFIG_PATH=$PROJECT_ROOT/config.txt
+
+# Directory wherein the application data is stored
+export STORAGE_FILES_PATH=$PROJECT_ROOT/volumes/
+
+# Create it if it doesn't exist
+if [ -z "$(ls $STORAGE_FILES_PATH 2>/dev/null)" ]; then
+  echo "Creating directory to hold the files... $STORAGE_FILES_PATH"
+  mkdir $STORAGE_FILES_PATH
+fi
+
+#################
+# Run the tests #
+#################
+
+make clean
+
+make tester
+
+./bin/tester
