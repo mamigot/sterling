a2778c96c0ca26cc03970479b929d9c086e2a0ae
Miguel Amigot <miguel.amigot@gmail.com>
2016-05-07 03:59:55 -0400

    Minor refactoring

--- storage/src/runserver.cpp
+++ storage/src/runserver.cpp
@@ -10,25 +10,25 @@ int getListenFD(const int port) {
   int listenfd;
   struct sockaddr_in servaddr;
 
-  // 1. Create the socket
+  // Create the socket
   if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
     perror("Unable to create a socket");
     exit(1);
   }
 
-  // 2. Set up the sockaddr_in
+  // Set up the sockaddr_in
   memset(&servaddr, 0, sizeof(servaddr)); // Zero it
   servaddr.sin_family = AF_INET; // Specify the family
   servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // Use any network card present
   servaddr.sin_port = htons(port); // Daytime server
 
-  // 3. "Bind" that address object to our listening file descriptor
+  // "Bind" that address object to our listening file descriptor
   if (::bind(listenfd, (SA *) &servaddr, sizeof(servaddr)) == -1) {
     perror("Unable to bind port");
     exit(2);
   }
 
-  // 4. Tell the system that we are going to use this socket for
+  // Tell the system that we are going to use this socket for
   // listening and request a queue length
   if (listen(listenfd, 1024) == -1) {
     perror("Unable to listen");
