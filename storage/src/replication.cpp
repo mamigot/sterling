80d3cf6233a24f1e5ad6fc890e0b5fe5714008d5
Miguel Amigot <miguel.amigot@gmail.com>
2016-05-09 23:12:12 -0400

    Tag messages from the primary to backup RMs with sequence numbers that are used in elections

--- storage/src/replication.cpp
+++ storage/src/replication.cpp
@@ -22,6 +22,8 @@ using namespace std;
 // Leader requests (RMs should only send these internally to the leader)
 regex rePrimaryRMRequest("PRIMARY/.*");
 
+regex reSequenceTaggedRequest("([0-9]+)/(.*)");
+
 // BOUNCE/PrimaryURequestPort\0
 regex reBounce("BOUNCE/([0-9]+)\0");
 
@@ -38,6 +40,104 @@ regex reSnapResponseStart(""); // TODO
 regex reSnapResponseEnd(""); // TODO
 
 enum RMStatus { Alive, Dead };
+
+class URequestLedger {
+public:
+  URequestLedger() : lastSequenceID(0) {}
+
+  void add(string& request) {
+    // If we're adding the request, we must have received it from the leader
+    // so it'll be preceeded by the leader's lastSequenceID... extract it
+    smatch matches;
+
+    if(regex_match(request, matches, reSequenceTaggedRequest)){
+      request = matches[2];
+      unsigned int newSequenceID = std::atoi(matches[1].str().c_str());
+
+      if(newSequenceID > lastSequenceID) lastSequenceID = newSequenceID;
+
+    }else{
+      string msg = "Request being processed by a backup is not tagged with the \
+        primary's lastSequenceID. Seen request: " + request;
+      throw std::runtime_error(msg);
+    }
+
+    unsigned int ledgerSize;
+    {
+      lock_guard<mutex> lck(ledgerMut);
+      ledger.push(request);
+
+      ledgerSize = ledger.size();
+    }
+
+    if(ledgerSize <= 1){
+      // If there's more than one element, another thread will be working to
+      // complete the requests. However, if this is the only element, then we
+      // should spin up another thread to process the request (and potentially
+      // future ones).
+      ledgerProcessor();
+    }
+  }
+
+  string next() {
+    lock_guard<mutex> lck(ledgerMut);
+
+    string next = ledger.front();
+    ledger.pop();
+
+    return next;
+  }
+
+  unsigned int size() {
+    lock_guard<mutex> lck(ledgerMut);
+    return ledger.size();
+  }
+
+  void broadcast(const string& request, vector<unsigned int>& iPorts) {
+    // Implements ordering (do not release the lock until all RMs have
+    // received the message)
+    unique_lock<mutex> lck(broadcastingMut);
+
+    vector<thread> unicasts;
+    for(auto& iPort:iPorts){
+      unicasts.push_back(
+        thread([iPort, request] { sendPort(request, iPort); })
+      );
+    }
+
+    // Wait until all sends have been completed to release the lock
+    for(thread& th:unicasts){ th.join(); }
+  }
+
+  unsigned int getLastSequenceID() const {
+    return (unsigned int) lastSequenceID;
+  }
+
+private:
+  queue<string> ledger;
+  mutex ledgerMut;
+
+  atomic<unsigned int> lastSequenceID;
+
+  // Use when broadcasting a message to other RMs
+  mutex broadcastingMut;
+
+  // Use while exchanging data with another RM (e.g. when resurrecting)
+  mutex inTransitMut;
+
+  void ledgerProcessor() {
+
+    thread([&] {
+
+      unique_lock<mutex> lck(inTransitMut);
+      while(size()){ parseURequest(next()); }
+
+    }).detach();
+  }
+
+};
+
+URequestLedger ledger;
 
 class RMVote {
 public:
@@ -84,15 +184,6 @@ private:
 };
 
 class RMSociety {
-/*
-RMSociety:
-- Tells me what my UREQUEST_PORT and IREQUEST_PORT are
-- Tells me who the leader is (and reflects changes if needed)
-- Has a method to refresh itself by sending heartbeats around
-- Tells me the statuses of every RM (refreshed periodically)
-- Has a method to tell me if a given RM is alive or dead
-*/
-
 public:
   RMSociety() {}
 
@@ -208,7 +299,8 @@ public:
   void vote(unsigned int electionIPort) {
     // If I'm asked to participate in somebody's election
     string fullVote = std::to_string(myIRequestPort) + "/ElectionResponse/" + \
-      std::to_string(lastSequenceID) + "/" + std::to_string(myURequestPort) + "\0";
+      std::to_string(ledger.getLastSequenceID()) + "/" + \
+      std::to_string(myURequestPort) + "\0";
 
     sendPort(fullVote, electionIPort);
     cerr << "Voted on " << electionIPort << "'s election: " << fullVote << endl;
@@ -243,7 +335,7 @@ public:
     unique_lock<mutex> lck(votesMut);
 
     // Choose a new leader (we're in the running too)
-    RMVote leader = RMVote(myURequestPort, lastSequenceID);
+    RMVote leader = RMVote(myURequestPort, ledger.getLastSequenceID());
 
     // Get the highest vote
     for(auto& vote:votes) {
@@ -274,7 +366,6 @@ private:
   atomic<unsigned int> myIRequestPort;
 
   atomic<unsigned int> primaryURequestPort;
-  atomic<unsigned int> lastSequenceID;
   //atomic<unsigned int> lastHeardFromPrimary;
 
   // Used to elect a new leader / primaryRM
@@ -291,83 +382,6 @@ private:
 
 RMSociety society;
 
-class URequestLedger {
-public:
-  void add(const string& request) {
-    unsigned int ledgerSize;
-    {
-      lock_guard<mutex> lck(ledgerMut);
-      ledger.push(request);
-
-      ledgerSize = ledger.size();
-    }
-
-    if(ledgerSize <= 1){
-      // If there's more than one element, another thread will be working to
-      // complete the requests. However, if this is the only element, then we
-      // should spin up another thread to process the request (and potentially
-      // future ones).
-      ledgerProcessor();
-    }
-  }
-
-  string next() {
-    lock_guard<mutex> lck(ledgerMut);
-
-    string next = ledger.front();
-    ledger.pop();
-
-    return next;
-  }
-
-  unsigned int size() {
-    lock_guard<mutex> lck(ledgerMut);
-    return ledger.size();
-  }
-
-  void broadcast(const string& request) {
-    // Ports of active backup RMs
-    auto iPorts = society.getIRequestPorts(RMStatus::Alive);
-
-    // Implements ordering (do not release the lock until all RMs have
-    // received the message)
-    unique_lock<mutex> lck(broadcastingMut);
-
-    vector<thread> unicasts;
-    for(auto& iPort:iPorts){
-      unicasts.push_back(
-        thread([iPort, request] { sendPort(request, iPort); })
-      );
-    }
-
-    // Wait until all sends have been completed to release the lock
-    for(thread& th:unicasts){ th.join(); }
-  }
-
-private:
-  queue<string> ledger;
-  mutex ledgerMut;
-
-  // Use when broadcasting a message to other RMs
-  mutex broadcastingMut;
-
-  // Use while exchanging data with another RM (e.g. when resurrecting)
-  mutex inTransitMut;
-
-  void ledgerProcessor() {
-
-    thread([&] {
-
-      unique_lock<mutex> lck(inTransitMut);
-      while(size()){ parseURequest(next()); }
-
-    }).detach();
-  }
-
-};
-
-URequestLedger ledger;
-
 string modifyRequestAsPrimaryRM(string& request) {
   // The contents of the request may need to change if it's being submitted
   // to the primary RM. This performs those checks and returns an updated one.
@@ -376,6 +390,9 @@ string modifyRequestAsPrimaryRM(string& request) {
   // If we're dealing with a savePost request, insert our time
   // (if this doesn't apply, won't change anything)
   request = insertTimestampIntoSavePostRequest(request);
+
+  // Add our sequence number to the request
+  request += std::to_string(ledger.getLastSequenceID());
 
   return request;
 }
@@ -392,13 +409,17 @@ void processURequest(string& request, const unsigned int connfd) {
   request = modifyRequestAsPrimaryRM(request);
 
   // Expect all backup RMs to implement it
-  if(isUpdateURequest(request)){ ledger.broadcast(request); }
+  if(isUpdateURequest(request)){
+    // Ports of active backup RMs
+    auto iPorts = society.getIRequestPorts(RMStatus::Alive);
+    ledger.broadcast(request, iPorts);
+  }
 
   // Process the request and get back to the user
   sendConn(parseURequest(request), connfd);
 }
 
-void processIRequest(const string& request, const unsigned int connfd) {
+void processIRequest(string& request, const unsigned int connfd) {
   // Request comes from another RM (it's internal)
 
   if(isURequest(request)){
@@ -436,126 +457,7 @@ void requestHandler(const unsigned int connfd, const unsigned int recvPort) {
 
   close(connfd);
   cerr << "Finished." << endl;
-
-  /*
-  I know on what port I received the request on so I know if it came from another
-  RM or a user.
-
-  If it's a request from a user and I'm not the leader: bounce!
-
-  If it's a request from a user and I'm the leader:
-    (if it's savePost, modify it to include my timestamp!)
-  - Add the request to requestLedger (for each backup)
-  - Tell backups to implement it (don't wait for their confirmation)
-  - Process the request
-  - Get back to the user with confirmation of completion
-
-  If it's a forwarded user request from an RM and I'm not the leader:
-  - Add the request to requestQueue
-  - Get back to the primary RM with confirmation of receipt (which allows
-    the leader to remove it from its ledger)
-  - (Another thread processes the requests on the requestQueue.)
-    if(requestQueue.length() <= 1 && !TRANSIT_MODE) { // TODO: lock before
-      // There'll be at least one because we just put one
-      // Create a new thread to process the requestQueue, one-by-one
-    }
-
-  If it's another kind of request from an RM and I'm not the leader: process it!
-
-  If it's a request from an RM and I'm the leader: process it!
-
-  // could be:
-  - user request (being sent from the primary for replication purposes)
-  - leadership inquiry (someone asking if i'm the leader)
-  - leadership announcement (someone telling me that he's the leader)
-  - a simple heartbeat
-  - snapshot request (only valid if I'm a leader; if i grant the request,
-    i go into TRANSIT_MODE which means that i have to queue my own requests
-    in addition to propagating them... i'd make the snapshot and return to
-    my duties. note that if i'm not the leader, i'd just deny it)
-  */
 }
-
-/*
-
-bool isAnRMPort(const unsigned int port) {
-  // Users will contact the user port. we want the internal port.
-  // Maybe this should be isInternalPort
-
-  // true iff the requested port belongs to the setup of RMs
-  lock_guard<rmStatusesMut> lck;
-
-  return rmStatuses.find(port) != rmStatuses.end();
-};
-
-void leadershipInquiry() {
-
-  // called every time the RM resurfaces
-
-  // asks everyone who the leader is and, if after a predetermined number of
-  // seconds there hasn't been a response, proclaims itself as the leader and
-  // tells everyone (when an RM proclaims itself as the leader, all others
-  // accept its claim... they assume that it has enough experience/data to lead)
-
-  // if a leader gets back to it, this asks the leader to replicate its data
-  // (the leader will regard this as a backup RM and will start to send user
-  // requests for its queue... however, whilst in TRANSIT_MODE, this RM will
-  // not process any of those requests)
-
-}
-
-void leadershipResponse() {
-
-  // if an RM receives a leadershipInquiry from another, it checks if it's
-  // the standing leader. if so, then it replies "no". if it's not the
-  // standing leader, it has no authority and thus doesn't say anything.
-
-}
-
-void leadershipAnnouncement() {
-
-  // if not in transit mode, the RM can tell everyone else that it's the leader.
-  // waits for their acknowledgements
-
-}
-
-void handleSnapshotRequest() {
-
-  // (only valid if I'm a leader; if i grant the request,
-  // i go into TRANSIT_MODE which means that i have to queue my own requests
-  // in addition to propagating them... i'd make the snapshot and return to
-  // my duties. note that if i'm not the leader, i'd just deny it)
-
-}
-
-void makeSnapshotRequest() {
-
-  // (in communication with the leader while it's all going on)
-
-  // set TRANSIT_MODE to true (when this happens, user requests should not
-  // be processed –just added to the ledger and the backups' queues)
-
-  // start a cp process from the srcPort to the destPort
-
-  // set TRANSIT_MODE to false
-
-}
-
-void heartbeatChecker() {
-
-  // used to refresh the RM's worldview in regards to the PRIMARY_RM_PORT,
-  // lastHeardFromPrimary and rmStatuses
-
-  // spin up one thread for each active RM in rmStatuses, send each a message
-  // and, if successful (failures because of busy sockets mean success),
-  // update their statuses on rmStatuses (also note failures, obviously).
-  // when checking the status of the primary RM, also update lastHeardFromPrimary
-
-  // thread_sleep() for HEARTBEAT_PERIOD_MS –this function will be running
-  // continuously
-
-}
-*/
 
 void configReplication() {
   // read topology.txt and set relevant port numbers
@@ -566,8 +468,6 @@ void configReplication() {
   // configure society and ledger
   // society = ...
   // ledger = ...
-
-
 
 
 }
